3131c09c7cfde0737ea0699e66e6106390453daa
James Allenby <JamesAllenby@protonmail.com>
2021-06-12 17:50:12 +0100

    Fixed lightwall collision between players

--- Assets/Scripts/Lightwall.cs
+++ Assets/Scripts/Lightwall.cs
@@ -33,7 +33,7 @@ public class Lightwall : IToggle
         Debug.Assert(m_MeshTransform != null, "unable to find lightwall mesh", this);
 
         // Collide with default non-special game objects
-        m_LayerMask = LayerMask.GetMask("Default", "Player");
+        m_LayerMask = LayerMask.GetMask("Wall");
         // Collide with lightwalls if we are ourselves a lightwall
         if (gameObject.layer == LayerMask.NameToLayer("Lightweight"))
         {
